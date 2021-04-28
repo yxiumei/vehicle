a45ce690d73f60b0b564d071631e1b4178c6822d
hjc <emailtome123456@163.com>
2021-04-28 15:40:44 +0800

    主键自增长

--- vehicle-domain/src/main/java/yxz/vehicle/model/entity/PointInfo.java
+++ vehicle-domain/src/main/java/yxz/vehicle/model/entity/PointInfo.java
@@ -1,6 +1,9 @@
 package yxz.vehicle.model.entity;
 
 import java.io.Serializable;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -19,6 +22,9 @@ import lombok.experimental.Accessors;
 public class PointInfo implements Serializable {
 
     private static final long serialVersionUID=1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
 
     /**
      * 经度
